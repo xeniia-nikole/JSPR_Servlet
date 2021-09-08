2437c88fcec787b9b9287ce18aa5c36796a5c91c
Kseniia <xenia-nikole@mail.ru>
2021-09-08 23:18:00 +0300

    do corrections in MainServlet

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -13,66 +13,51 @@ import java.io.IOException;
 public class MainServlet extends HttpServlet {
   private PostController controller;
   final private static String API = "/api/posts";
-  final private static String API_D = "/api/posts/\\d+";
+  final private static String API_ID = "/api/posts/\\d+";
 
   @Override
   public void init() {
     final var repository = new PostRepository();
+
     final var service = new PostService(repository);
     controller = new PostController(service);
+
   }
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-    try {
-      final var path = req.getRequestURI();
-      final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-
-      if (path.equals(API)) {
-        controller.all(resp);
-        return;
-      }
-      if (path.matches(API_D)) {
-        controller.getById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final var path = req.getRequestURI();
+    if (path.equals(API)) {
+      controller.all(resp);
+      return;
+    } else if (path.matches(API_ID)) {
+      final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+      controller.getById(id, resp);
+      return;
     }
+    super.doGet(req, resp);
   }
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-    try {
-      final var path = req.getRequestURI();
-
-      if (path.equals(API)) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final var path = req.getRequestURI();
+    if (path.equals(API)) {
+      controller.save(req.getReader(), resp);
+      return;
     }
+    super.doPost(req, resp);
   }
 
   @Override
-  public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    try {
-      final var path = req.getRequestURI();
-      final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+  protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    final var path = req.getRequestURI();
 
-      if (path.matches(API_D)) {
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    if (path.matches(API_ID)) {
+
+      final var id = Long.parseLong(path.substring(path.lastIndexOf("/")+1));
+      controller.removeById(id, resp);
+      return;
     }
+    super.doDelete(req, resp);
   }
 }
